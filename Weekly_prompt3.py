e1bc689773c560b1011770d82d93b0875eaaeba9
gamath99 <gamamathurin1@gmail.com>
2026-06-21 15:01:33 -0400

    Used of print function to display the text

--- Weekly_prompt3.py
+++ Weekly_prompt3.py
@@ -1,3 +1,6 @@
-"Subject 2"
-"Pick a S.O.L.I.D design principle and explain the concept and how you can meet that principle when designing a program."
-
+print(""" Subject 2
+      Pick a S.O.L.I.D design principle and explain the concept and how you can meet that principle when designing a program.
+In Object-oriented programming,  a class can be refer as a blueprint used to create object that share common properties and behavior. It can be created by using the Class keyword followed by te class name.
+To unsure a better fluidity when using class, it is necessary to consider the mnemonic acronym S.O.L.I.D. It refers to fie principles intended to make source code more understanble, fleible, and maintainable.
+The five principles are singles responsibility principle, open-closed principle, Liskov substitution principle, Interface segregation principle and Depency inversion principle.
+""")
